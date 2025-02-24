0000aa13e5020922056693b97d13e2afc0ae1053
Chris Herrera <chris@theops.dev>
2025-02-23 22:37:16 -0800

    Adding apply logic to workflow

--- opentofu/digitalocean/droplets.tf
+++ opentofu/digitalocean/droplets.tf
@@ -1,7 +1,7 @@
 module "k3s-controlplane" {
   source = "../tofu-modules/do-droplet"
 
-  name         = "k3s-controlplane-1"
+  name         = "k3s-control-1"
   size         = "basic-xs"
   storage_size = 50 # GB
 
