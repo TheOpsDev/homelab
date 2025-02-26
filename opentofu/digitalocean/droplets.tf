429ee3b3957b9ea768f1ebc058c4c66e7e8f8967
Chris Herrera <christian@christian-herrera.com>
2025-02-25 20:12:59 -0800

    Provisioning a FW for k3s cluster (#52)
    * Provisioning a FW for k3s cluster
    
    * Adding droplet ids
    
    ---------
    
    Co-authored-by: Chris Herrera <chris@theops.dev>

--- opentofu/digitalocean/droplets.tf
+++ opentofu/digitalocean/droplets.tf
@@ -1,4 +1,8 @@
-## Control Plane
+locals {
+  droplet_ids = [for id in data.digitalocean_droplets.k3s_cluster.droplets[*].id : id]
+}
+
+# Control Plane
 module "k3s-controlplane" {
   source = "../tofu-modules/do-droplet"
 
@@ -8,4 +12,15 @@ module "k3s-controlplane" {
 
   vpc_id = digitalocean_vpc.homelab.id
   tags   = ["k3s", "controlplane"]
+}
+
+data "digitalocean_droplets" "k3s_cluster" {
+  filter {
+    key    = "tags"
+    values = ["k3s"]
+  }
+  sort {
+    key       = "created_at"
+    direction = "desc"
+  }
 }
