d2673b0fec51885ddd738d587461ed000d7c1aa5
Chris Herrera <chris@theops.dev>
2025-02-25 20:38:16 -0800

    Adding dns records for k3s

--- opentofu/digitalocean/dns.tf
+++ opentofu/digitalocean/dns.tf
@@ -7,5 +7,5 @@ resource "digitalocean_record" "k3s_theopsdev" {
   domain = digitalocean_domain.theopsdev.name
   type   = "A"
   name   = "k3s"
-  value  = module.k3s-controlplane.ipv4_address
+  value  = sensitive(module.k3s-controlplane.ipv4_address)
 }
