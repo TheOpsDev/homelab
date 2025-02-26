a581b79c942f9872c8f71ae7eee526d71981491a
Chris Herrera <chris@theops.dev>
2025-02-25 20:36:57 -0800

    Adding dns records for k3s

---
+++ opentofu/tofu-modules/do-droplet/outputs.tf
@@ -0,0 +1,19 @@
+output "ipv4_address" {
+  value = digitalocean_droplet.opsdev.ipv4_address
+}
+
+output "name" {
+  value = digitalocean_droplet.opsdev.name
+}
+
+output "status" {
+  value = digitalocean_droplet.opsdev.status
+}
+
+output "id" {
+  value = digitalocean_droplet.opsdev.id
+}
+
+output "urn" {
+  value = digitalocean_droplet.opsdev.urn 
+}
