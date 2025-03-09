f1fb96ba1cb9f5e7705a435a45d963e241794ddc
Chris Herrera <chris@theops.dev>
2025-03-08 23:37:38 -0800

    Opening firewall to udp#53

--- opentofu/digitalocean/vpc.tf
+++ opentofu/digitalocean/vpc.tf
@@ -1,6 +1,6 @@
 locals {
   k3s_cluster_fw = {
-    inbound_ports  = ["22", "6443", "80", "443"]
+    inbound_ports  = ["22", "6443", "80", "443", "53"]
     outbound_ports = ["53", "80", "443", "6443"]
   }
 }
@@ -31,5 +31,17 @@ resource "digitalocean_firewall" "k3s_cluster" {
       port_range            = outbound_rule.value
       destination_addresses = ["0.0.0.0/0", "::/0"]
     }
+  }
+
+  inbound_rule {
+    protocol         = "udp"
+    port_range       = "53"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
