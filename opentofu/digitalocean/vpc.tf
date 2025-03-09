dc26e060479e638f102b48b07954abf381d67a55
Chris Herrera <christian@christian-herrera.com>
2025-03-08 22:48:38 -0800

    Setup and deploy k3s to remote lab (#54)
    Co-authored-by: Chris Herrera <chris@theops.dev>

--- opentofu/digitalocean/vpc.tf
+++ opentofu/digitalocean/vpc.tf
@@ -1,7 +1,7 @@
 locals {
   k3s_cluster_fw = {
-    inbound_ports =[ "22", "6443", "80", "443" ]
-    outbound_ports = [ "53", "80", "443" ]
+    inbound_ports  = ["22", "6443", "80", "443"]
+    outbound_ports = ["53", "80", "443", "6443"]
   }
 }
 
@@ -12,14 +12,14 @@ resource "digitalocean_vpc" "homelab" {
 }
 
 resource "digitalocean_firewall" "k3s_cluster" {
-  name = "k3s-cluster"
+  name        = "k3s-cluster"
   droplet_ids = local.droplet_ids
 
   dynamic "inbound_rule" {
     for_each = local.k3s_cluster_fw.inbound_ports
     content {
-      protocol = "tcp"
-      port_range = inbound_rule.value
+      protocol         = "tcp"
+      port_range       = inbound_rule.value
       source_addresses = sensitive(var.source_cidrs)
     }
   }
@@ -27,8 +27,8 @@ resource "digitalocean_firewall" "k3s_cluster" {
   dynamic "outbound_rule" {
     for_each = local.k3s_cluster_fw.outbound_ports
     content {
-      protocol = "tcp"
-      port_range = outbound_rule.value
+      protocol              = "tcp"
+      port_range            = outbound_rule.value
       destination_addresses = ["0.0.0.0/0", "::/0"]
     }
   }
