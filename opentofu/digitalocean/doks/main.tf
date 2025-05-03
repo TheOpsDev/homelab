d764b0e6619bd5e6317e9d1a15a5184fc130a3e3
Chris Herrera <chris@theops.dev>
2025-05-02 23:23:54 -0700

    Provisioning DOKS cluster

--- opentofu/digitalocean/doks/main.tf
+++ opentofu/digitalocean/doks/main.tf
@@ -1,18 +1,42 @@
-resource "digitalocean_kubernetes_cluster" "foo" {
-  name   = "foo"
-  region = "nyc1"
-  # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.22.8-do.1"
+locals {
+  tags = [
+    "k8s",
+    "homelab",
+  ]
+  labels = {
+    "environment" = "homelab"
+    "component"   = "critical"
+    "app"         = "k8s"
+    "owner"       = "theopsdev"
+  }
+}
+
+data "digitalocean_vpc" "homelab" {
+  name = "homelab-network"
+}
+
+resource "digitalocean_kubernetes_cluster" "homelab" {
+  name    = "homelab"
+  region  = "sfo3"
+  version = "1.32.2-do.0"
+
+  vpc_uuid = data.digitalocean_vpc.homelab.id
+  tags     = local.tags
 
   node_pool {
-    name       = "worker-pool"
-    size       = "s-2vcpu-2gb"
-    node_count = 3
+    name       = "default"
+    size       = "s-2vcpu-4gb"
+    auto_scale = true
+    min_nodes  = 2
+    max_nodes  = 4
 
-    taint {
-      key    = "workloadKind"
-      value  = "database"
-      effect = "NoSchedule"
-    }
+    tags = local.tags
+    labels = local.labels
+  }
+
+  auto_upgrade = true
+  maintenance_policy {
+    day          = "sunday"
+    start_time = "01:00"
   }
 }
